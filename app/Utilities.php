dd1250284fa4c5c412a80c094e0cdb153390e118
qaynam <33139713+qaynam@users.noreply.github.com>
2022-08-09 20:43:50 +0900

    Update Utilities.php

--- app/Utilities.php
+++ app/Utilities.php
@@ -18,5 +18,14 @@ class Utilities {
         
         die();
     }
+    
+    /**
+     * サイトのURLを取得する
+    */
+    public static function siteURL() {
+        $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
+        $domainName = $_SERVER['HTTP_HOST'].'/';
+        return $protocol.$domainName;
+    }
 
 }
